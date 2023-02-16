12331d7b359fad9fef09120337476cdad5863d17
Sshesst <dimciksalkov1832@gmail.com>
2023-02-17 01:31:09 +0500

    I just refused from  MaterialUI

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,63 +1,9 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  // Button,
-  IconButton,
-  makeStyles,
-  Container
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import HomeIcon from "@material-ui/icons/Home";
-import React from "react";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
+import React from 'react'
 
 const Nav = () => {
-  const classes = useStyles();
-
   return (
-    <Container fixed>
-      <div className={classes.root}>
-        <AppBar
-          position="static"
-          style={{ background: "#e0d1f8", borderRadius: "4px" }}
-        >
-          <Toolbar color="">
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h4" className={classes.title}>
-              {/* Портфолио */}
-            </Typography>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <HomeIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </Container>
-  );
-};
+    <div>Nav</div>
+  )
+}
 
-export default Nav;
+export default Nav
